12839340e7091006a68d702cdabbb355aff74618
Rafael <rafael0520@gmail.com>
2020-04-14 10:41:34 -0300

    New Third Person Controller
    New Third Person Controller

---
+++ Assets/Codes/ThirdCam.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdCam : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset, lookOffset;
+    public float scrollOffset;
+
+    GameObject fakeObject;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        fakeObject = new GameObject();
+    }
+
+    public GameObject GetReferenceObject()
+    {
+        return fakeObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fakeObject.transform.position = Vector3.Lerp(fakeObject.transform.position, player.transform.position, Time.deltaTime * 5);
+        transform.position = fakeObject.transform.position + fakeObject.transform.forward * offset.z + fakeObject.transform.up * offset.y;
+
+        transform.LookAt(player.transform.position + lookOffset);
+
+        scrollOffset = Mathf.Clamp(scrollOffset + Input.mouseScrollDelta.y, -6, -2);
+        offset = new Vector3(0, offset.y, scrollOffset);
+
+        fakeObject.transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0));
+    }
+}
---
+++ Assets/Codes/ThirdWalk.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdWalk : MonoBehaviour
+{
+    public enum States
+    {
+        Idle,
+        Walk,
+        Jump,
+        Die
+    }
+
+    public States state;
+    public Animator anim;
+    public Rigidbody rb;
+
+    public Vector3 move { get; private set; }
+    public float moveForce = 50f;
+    public float speed = 20f;
+    private Vector3 direction;
+    private GameObject camRefObj;
+
+    void Start()
+    {
+        camRefObj = Camera.main.GetComponent<ThirdCam>().GetReferenceObject();
+        StartCoroutine(Idle());
+    }
+
+    void FixedUpdate()
+    {
+        //criacao de vetor de movimento local
+        move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        move = camRefObj.transform.TransformDirection(move);
+
+        //girar pra direcao das teclas
+        if (move.magnitude > 0)
+        {
+            direction = move;
+        }
+        transform.forward = Vector3.Lerp(transform.forward, direction, Time.fixedDeltaTime * 10);
+
+        //rb.AddForce(move * (moveForce / (rb.velocity.magnitude + 1))); //* speed);
+        rb.AddForce(move * moveForce * speed);
+    }
+
+    IEnumerator Idle()
+    {
+        //equivalente ao Start
+        state = States.Idle;
+
+
+        //
+        while (state == States.Idle)
+        {
+            //equivalente ao Update
+            anim.SetFloat("Velocity", rb.velocity.magnitude);
+
+            if(rb.velocity.magnitude > 0.1)
+            {
+                StartCoroutine(Walk());
+            }
+
+            //
+            yield return new WaitForEndOfFrame();
+        }
+        //saida do estado
+    }
+
+    IEnumerator Walk()
+    {
+        //equivalente ao Start
+        state = States.Walk;
+
+
+        //
+        while (state == States.Walk)
+        {
+            //equivalente ao Update
+            anim.SetFloat("Velocity", rb.velocity.magnitude);
+
+            if (rb.velocity.magnitude < 0.1)
+            {
+                StartCoroutine(Idle());
+            }
+
+
+            //
+            yield return new WaitForEndOfFrame();
+        }
+        //saida do estado
+    }
+}
